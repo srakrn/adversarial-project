be2a314a039a56cad238d0b11f323d73291f8a28
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-11-07 00:51:51 +0700

    :tada: Heavy amount of initial works
    Former-commit-id: c67468d24e315b637328e9dc1d88e0185b62a8e4 [formerly cec769614e605c17042203268c09c89b91ef5276] [formerly c67468d24e315b637328e9dc1d88e0185b62a8e4 [formerly cec769614e605c17042203268c09c89b91ef5276] [formerly bd9adea1cdbf78b5b2cb27a5850f7b7b4c4f6fbb [formerly 2d621cc70d45b8f5d1dcffe31cda84c1c0f0296a]]]
    Former-commit-id: bd9adea1cdbf78b5b2cb27a5850f7b7b4c4f6fbb
    Former-commit-id: 6bda093fb161ed79c8a17221ca02e222d837de94 [formerly 35e99fbe5a0bf0a09b90490fa5b16149c52b4c0a]
    Former-commit-id: aef6dcbcb0c235132b641b838ca9bcff130643a4

---
+++ cnn_hypothesis_evaluation.py
@@ -0,0 +1,137 @@
+#%%
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from sklearn.metrics import confusion_matrix 
+
+#%%
+torch.manual_seed(0)
+
+# %%
+transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+)
+mnist_trainset = datasets.MNIST(
+    root="mnist", train=True, download=True, transform=transform
+)
+mnist_testset = datasets.MNIST(
+    root="mnist", train=False, download=True, transform=transform
+)
+
+# %%
+class MnistCnn(nn.Module):
+    def __init__(self):
+        super(MnistCnn, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(8, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+# %%
+model = MnistCnn()
+mnist_state = torch.load("models/mnist_cnn.model")
+model.load_state_dict(mnist_state)
+
+# %%
+fcnn_perturbs = torch.load("models/fcnn_perturbs.model")
+cnn_perturbs = torch.load("models/cnn_perturbs.model")
+
+# %%
+fig, axs = plt.subplots(2, 5, figsize=(10, 4))
+for perturb, ax in zip(fcnn_perturbs, axs.ravel()):
+    ax.imshow(perturb.detach().reshape(28, 28))
+plt.suptitle("Adversarial perturbations (FCNN)")
+plt.show()
+
+# %%
+fig, axs = plt.subplots(2, 5, figsize=(10, 4))
+for perturb, ax in zip(cnn_perturbs, axs.ravel()):
+    ax.imshow(perturb.detach().reshape(28, 28))
+plt.suptitle("Adversarial perturbations (CNN)")
+plt.show()
+
+# %%
+n_perturbs = 0
+perturbs_array = "fcnn_perturbs"
+density = 0.2
+perturb = eval(perturbs_array)[n_perturbs].reshape(1, 28, 28)
+
+# %%
+n_training_samples = 0
+training_feature, training_target = mnist_trainset[n_training_samples]
+print(training_target)
+
+# %%
+attacked_training_feature = training_feature + density * perturb
+attacked_training_feature = attacked_training_feature.reshape(1, 1, 28, 28)
+
+# %%
+predicted_target = model(attacked_training_feature).argmax()
+
+fig, axs = plt.subplots(1, 3, figsize=(9, 3))
+axs[0].imshow(training_feature.numpy().reshape(28, 28))
+axs[0].set_title("Training point #{}".format(n_training_samples))
+axs[1].imshow(perturb.detach().numpy().reshape(28, 28))
+axs[1].set_title("{}[{}]".format(perturbs_array, n_perturbs))
+axs[2].imshow(attacked_training_feature.detach().numpy().reshape(28, 28))
+axs[2].set_title("Adversarial image")
+plt.suptitle("Predicted as {}".format(predicted_target))
+plt.show()
+# %%
+x_legend = np.linspace(-1, 1, 101)
+attacked_images = []
+for i in x_legend:
+    attacked_images.append(i * perturb + training_feature)
+model_probs = []
+for i in attacked_images:
+    model_probs.append(model(i.view(-1, 1, 28, 28)).detach().numpy())
+model_probs = np.array(model_probs).reshape(101, 10).T
+
+for i, v in enumerate(model_probs):
+    plt.plot(x_legend, v, label="{}".format(i))
+
+plt.legend()
+plt.show()
+
+# %%
+predicted_targets = model(mnist_testset.data.float().reshape(-1, 1, 28, 28)).argmax(axis=1).detach().numpy()
+actual_targets = mnist_testset.targets.numpy()
+print("Model's confusion matrix")
+cm = confusion_matrix(actual_targets, predicted_targets)
+print(cm)
+
+# %%
+predicted_adversarial_targets = (
+    model(mnist_testset.data.float().reshape(-1, 1, 28, 28) + (density * perturb).reshape(1, 1, 28, 28))
+    .argmax(axis=1)
+    .detach()
+    .numpy()
+)
+actual_targets = mnist_testset.targets.numpy()
+print("Model's adversarial confusion matrix")
+adversarial_cm = confusion_matrix(actual_targets, predicted_adversarial_targets)
+print(adversarial_cm)
+
+# %%
+print("Adversarial and non-adversarial differences")
+print(adversarial_cm - cm)
+
+# %%
