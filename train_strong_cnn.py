e71c4738d6565107ba9b089480a48bea356593a1
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-11-09 15:43:22 +0700

    Training strong model
    Former-commit-id: 903382a2052468313d4b6ed2af2703322fde9ff6 [formerly c4370de6326da633ef863f129d7023c339f313fd] [formerly 903382a2052468313d4b6ed2af2703322fde9ff6 [formerly c4370de6326da633ef863f129d7023c339f313fd] [formerly c52fec78de791f1d0873b91e735a96234b9b5a4c [formerly 7319275f0c659d1d74e0824d1c3edb29a082d439]]]
    Former-commit-id: c52fec78de791f1d0873b91e735a96234b9b5a4c
    Former-commit-id: 7220d0262ff2bc24d08662ae953ec824f31cbd33 [formerly a3f359edd3d9465475c03006718c2d416324f6d4]
    Former-commit-id: 3e2811bac00bef43366a6fe30c2a993bc1f6a462

---
+++ train_strong_cnn.py
@@ -0,0 +1,137 @@
+#%%
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+
+#%%
+torch.manual_seed(0)
+
+#%%
+transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+)
+mnist_trainset = datasets.MNIST(
+    root="mnist", train=True, download=True, transform=transform
+)
+mnist_testset = datasets.MNIST(
+    root="mnist", train=False, download=True, transform=transform
+)
+trainloader = DataLoader(mnist_trainset, batch_size=64, shuffle=True)
+testloader = DataLoader(mnist_testset, batch_size=1, shuffle=True)
+
+# %%
+class MnistCnn(nn.Module):
+    def __init__(self):
+        super(MnistCnn, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(8, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+# %%
+model = MnistCnn()
+mnist_state = torch.load("models/mnist_cnn.model")
+model.load_state_dict(mnist_state)
+
+# %%
+if os.path.exists("models/cnn_fullset_perturbs.model"):
+    print("Loading pre-existed perturbations")
+    perturbs = torch.load("models/cnn_fullset_perturbs.model")
+    perturbs = list(perturbs)
+else:
+    print("Creating new set of perturbation")
+    perturbs = []
+densities = [-0.05, 0.05]
+
+#%%
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+init_rounds = len(perturbs)
+total_rounds = 9
+
+#%%
+for r in range(init_rounds, init_rounds + total_rounds):
+    print("Round:", r + 1)
+    # Fetch one attack image
+
+    # Create a random array of perturbation
+    perturb = torch.zeros([1, 1, 28, 28], requires_grad=True)
+
+    # Epsilon defines the maximum density (-e, e). It should be
+    # in the range of the training set's scaled value.
+    epsilon = 1
+
+    adversarial_optimizer = optim.SGD([perturb], lr=0.1)
+
+    # Train the adversarial noise, maximising the loss
+    epochs = 3
+    for e in range(epochs):
+        running_loss = 0
+        for images, labels in trainloader:
+            adversarial_optimizer.zero_grad()
+
+            output = model(images + perturb)
+            loss = -criterion(output, labels)
+            loss.backward()
+            adversarial_optimizer.step()
+            running_loss += loss.item()
+            perturb.data.clamp_(-epsilon, epsilon)
+        print("\tNoise loss:", -1 * running_loss / len(trainloader))
+
+    # Save the perturbations
+    perturbs.append(perturb)
+
+    # Train the model again with all perturbations added.
+    epochs = 3
+    for e in range(epochs):
+        running_loss = 0
+        for images, labels in trainloader:
+            for perturb in perturbs:
+                for density in densities:
+                    optimizer.zero_grad()
+
+                    output = F.log_softmax(model(images + density * perturb), dim=1)
+                    loss = criterion(output, labels)
+                    loss.backward()
+                    optimizer.step()
+
+                    running_loss += loss.item()
+        else:
+            print("\tTraining loss: {}".format(running_loss / len(trainloader)))
+    torch.save(
+        model.state_dict(), "models/mnist_cnn_reinforced_{}.model".format(r + 1)
+    )
+
+# %%
+perturbs = torch.stack(perturbs)
+torch.save(perturbs, "models/cnn_fullset_perturbs.model")
+
+# %%
+fig, axs = plt.subplots(2, 5, figsize=(10, 4))
+for p, ax in zip(perturbs, axs.ravel()):
+    ax.imshow(p.detach().numpy().reshape(28, 28))
+plt.show()
+
+# %%
+print("Hello?")
+
+# %%
