d6936d4a136b4e58cfd5c8dc1a7ae7c38a51844d
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-11-09 15:43:10 +0700

    Code for visualising muti class attacks
    Former-commit-id: 920768eba3fe524a33e03a4635cfb27d8d3fe525 [formerly 74543a3e5d54687778cccea1306eb44149285386] [formerly 920768eba3fe524a33e03a4635cfb27d8d3fe525 [formerly 74543a3e5d54687778cccea1306eb44149285386] [formerly 657105f5b0d1d8a57a78e002069e4b0ffb757686 [formerly c42cde2dbbe1e6a606626ac147bced8b481539b6]]]
    Former-commit-id: 657105f5b0d1d8a57a78e002069e4b0ffb757686
    Former-commit-id: ee5339fae95d27c9058f6f49fee26b07a149109b [formerly 236de57f21d07feb3e52c498e225af1b81ce5edc]
    Former-commit-id: eec192a5497f9af23ed958089840a0511cefb456

--- multi_class_adversarial_on_cnn.py
+++ multi_class_adversarial_on_cnn.py
@@ -26,24 +26,28 @@ trainloader = DataLoader(mnist_trainset, batch_size=64, shuffle=True)
 testloader = DataLoader(mnist_testset, batch_size=1, shuffle=True)
 
 # %%
-class MnistFcnn(nn.Module):
+class MnistCnn(nn.Module):
     def __init__(self):
-        super(MnistFcnn, self).__init__()
-        self.linear1 = nn.Linear(784, 128)
-        self.linear2 = nn.Linear(128, 64)
-        self.linear3 = nn.Linear(64, 10)
+        super(MnistCnn, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(8, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = x.reshape(-1, 28 * 28)
-        x = F.relu(self.linear1(x))
-        x = F.relu(self.linear2(x))
-        x = self.linear3(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
-
 # %%
-model = MnistFcnn()
-mnist_state = torch.load("models/mnist_fcnn.model")
+model = MnistCnn()
+mnist_state = torch.load("models/mnist_cnn.model")
 model.load_state_dict(mnist_state)
 
 # %%
@@ -86,24 +90,29 @@ optimizer = optim.SGD(model.parameters(), lr=0.01)
 
 #%%
 perturbs = []
+model = model.cuda()
+
 for i in nth_training_points:
     print("Index:", i)
     # Fetch one attack image
     attack_image, attack_label = mnist_trainset[i]
-    feeding_attack_image = attack_image.reshape(1, -1)
+    feeding_attack_image = attack_image.reshape(1, 1, 28, 28)
     feeding_attack_label = torch.tensor([attack_label])
 
+    feeding_attack_image = feeding_attack_image.cuda()
+    feeding_attack_label = feeding_attack_label.cuda()
+
     # Create a random array of perturbation
-    perturb = torch.zeros([1, 28 * 28], requires_grad=True)
+    perturb = torch.zeros([1, 1, 28, 28], requires_grad=True, device='cuda')
 
     # Epsilon defines the maximum density (-e, e). It should be
     # in the range of the training set's scaled value.
     epsilon = 1
 
-    adversarial_optimizer = optim.SGD([perturb], lr=0.1)
+    adversarial_optimizer = optim.SGD([perturb], lr=0.05)
 
     # Train the adversarial noise, maximising the loss
-    epochs = 10000
+    epochs = 20000
     for e in range(epochs):
         running_loss = 0
         adversarial_optimizer.zero_grad()
@@ -121,13 +130,13 @@ for i in nth_training_points:
 
 # %%
 perturbs = torch.stack(perturbs)
-torch.save(perturbs, "models/multiclass_fcnn_perturbs.model")
+torch.save(perturbs, "models/multiclass_cnn_perturbs.model")
 
 # %%
-perturbs = torch.load("models/multiclass_fcnn_perturbs.model")
+perturbs = torch.load("models/multiclass_cnn_perturbs.model")
 fig, axs = plt.subplots(10, 3, figsize=(3, 10))
 for p, ax in zip(perturbs, axs.ravel()):
-    ax.imshow(p.detach().numpy().reshape(28, 28))
+    ax.imshow(p.cpu().detach().numpy().reshape(28, 28))
     ax.axis('off')
 plt.show()
 
