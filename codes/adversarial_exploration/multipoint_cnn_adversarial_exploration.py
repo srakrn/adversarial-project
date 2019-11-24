57f08e3237e9c42621c4880e60aa13a271bb0302
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-11-24 20:27:27 +0700

    CNN perturbations exploration
    Former-commit-id: 989581b8c799cb77926c8458a89f6b443ba7bbee [formerly 71f1273db0d72e5e38ca8403a25d1aeb7aec7c8c] [formerly 989581b8c799cb77926c8458a89f6b443ba7bbee [formerly 71f1273db0d72e5e38ca8403a25d1aeb7aec7c8c] [formerly 75da3ff72daa4e359b95c59e2cad39bf58f35e59 [formerly 73e324b3f9314b272066671a4e08e4bdf873ee4d]]]
    Former-commit-id: 75da3ff72daa4e359b95c59e2cad39bf58f35e59
    Former-commit-id: 45a7d96af3e36f65bd61e9e0dc1ddadc312e5b20 [formerly e2a66c290a57605b61580902d5d75c3541d78e2c]
    Former-commit-id: df3b1501bdee763e0804c22e39b72c1d6681aaa9

---
+++ codes/adversarial_exploration/multipoint_cnn_adversarial_exploration.py
@@ -0,0 +1,111 @@
+#%%
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from sklearn.metrics import classification_report, confusion_matrix
+
+#%%
+torch.manual_seed(0)
+
+#%%
+transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+)
+mnist_trainset = datasets.MNIST(
+    root="mnist", train=True, download=True, transform=transform
+)
+mnist_testset = datasets.MNIST(
+    root="mnist", train=False, download=True, transform=transform
+)
+trainloader = DataLoader(mnist_trainset, batch_size=60000, shuffle=False)
+testloader = DataLoader(mnist_testset, batch_size=10000, shuffle=False)
+
+# %%
+class MnistCnn(nn.Module):
+    def __init__(self):
+        super(MnistCnn, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(8, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+# %%
+model = MnistCnn()
+mnist_state = torch.load("models/mnist_cnn.model")
+model.load_state_dict(mnist_state)
+
+# %%
+perturbs = torch.load("perturbs/on_single_point/cnn_on_single_point.pt")
+perturbs = perturbs.reshape(10000, 1, 28, 28)
+
+# %%
+density = 0.1
+x_test, y_test = next(iter(testloader))
+x_test = x_test.reshape(-1, 1, 28, 28)
+x_adver = x_test + density * perturbs
+y_pred = model(x_test).argmax(dim=1).numpy().astype(int)
+y_pred_adver = model(x_test + x_adver).argmax(dim=1).detach().numpy().astype(int)
+y_pred_perturbs = model(perturbs).argmax(dim=1).detach().numpy().astype(int)
+
+# %%
+report = classification_report(y_test, y_pred)
+print("Classification report of model")
+print(report)
+
+# %%
+adver_report = classification_report(y_test, y_pred_adver)
+print("Classification report of model (adversarial)")
+print(adver_report)
+
+# %%
+perturbs_report = classification_report(y_test, y_pred_perturbs)
+print("Classification report of model (only perturbs)")
+print(perturbs_report)
+
+# %%
+clf_mat = confusion_matrix(y_test, y_pred)
+print("Confusion matrix: True and Predicted")
+print(clf_mat)
+
+# %%
+adver_clf_mat = confusion_matrix(y_test, y_pred_adver)
+print("Confusion matrix: True and Adversarial Predicted")
+print(adver_clf_mat)
+
+# %%
+print("Confusion matrix difference: Adversarial - Predicted")
+print(adver_clf_mat - clf_mat)
+
+# %%
+print("Confusion matrix: True and Perturbations")
+perturbs_clf_mat = confusion_matrix(y_test, y_pred_perturbs)
+print(perturbs_clf_mat)
+
+# %%
+print("Confusion matrix difference: Adversarial - Perturbations")
+print(adver_clf_mat - perturbs_clf_mat)
+
+
+# %%
+np.savetxt("models/classification_results/mnist_cnn_testset.txt", y_pred)
+np.savetxt("models/classification_results/mnist_cnn_testset_adver.txt", y_pred_adver)
+np.savetxt("models/classification_results/mnist_cnn_testset_perturbs.txt", y_pred_perturbs)
+
+# %%
