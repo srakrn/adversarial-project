558f90f2a4ad0040a94d67c65f235f4383e40803
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-11-24 21:41:18 +0700

    Training results output (NumPy savetxt format)
    Former-commit-id: 36931c81720ec674c47fd8c023e1ad3dc469109c [formerly d415b280d354fb79888b1cf83abcf69b2e3988ae] [formerly 36931c81720ec674c47fd8c023e1ad3dc469109c [formerly d415b280d354fb79888b1cf83abcf69b2e3988ae] [formerly 131af86bf719725a598c4ac5f8a16fd4a9d251f9 [formerly e802556a595d98e2f852ebe9a592796482573426]]]
    Former-commit-id: 131af86bf719725a598c4ac5f8a16fd4a9d251f9
    Former-commit-id: f9724f2fdd847b2fb815910b519226e39a42ce34 [formerly 818def0c740569a2dd9c7671e6c184d7de6eb409]
    Former-commit-id: 8f6e76754f0dc90a8316847de217be946006ab9e

--- codes/adversarial_exploration/on_single_point/cnn_perturbs_on_fcnn.py
+++ codes/adversarial_exploration/on_single_point/cnn_perturbs_on_fcnn.py
@@ -15,7 +15,7 @@ torch.manual_seed(0)
 
 #%%
 transform = transforms.Compose(
-    [transforms.Normalize((0.5,), (0.5,))]
+    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
 )
 mnist_trainset = datasets.MNIST(
     root="mnist", train=True, download=True, transform=transform
@@ -27,38 +27,33 @@ trainloader = DataLoader(mnist_trainset, batch_size=60000, shuffle=False)
 testloader = DataLoader(mnist_testset, batch_size=10000, shuffle=False)
 
 # %%
-class MnistCnn(nn.Module):
+class MnistFcnn(nn.Module):
     def __init__(self):
-        super(MnistCnn, self).__init__()
-        self.conv1 = nn.Conv2d(1, 8, 3)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(8, 16, 5)
-        self.fc1 = nn.Linear(16 * 4 * 4, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        super(MnistFcnn, self).__init__()
+        self.linear1 = nn.Linear(784, 128)
+        self.linear2 = nn.Linear(128, 64)
+        self.linear3 = nn.Linear(64, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 4 * 4)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = x.reshape(-1, 28 * 28)
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
         return x
 
 # %%
-model = MnistCnn()
-mnist_state = torch.load("models/mnist_cnn.model")
+model = MnistFcnn()
+mnist_state = torch.load("models/mnist_fcnn.model")
 model.load_state_dict(mnist_state)
 
 # %%
 perturbs = torch.load("perturbs/on_single_point/cnn_on_single_point.pt")
-perturbs = perturbs.reshape(10000, 1, 28, 28)
+perturbs = perturbs.reshape(10000, 28, 28)
 
 # %%
 density = 0.1
 x_test, y_test = next(iter(testloader))
-x_test = x_test.reshape(-1, 1, 28, 28)
+x_test = x_test.reshape(-1, 28, 28)
 x_adver = x_test + density * perturbs
 y_pred = model(x_test).argmax(dim=1).numpy().astype(int)
 y_pred_adver = model(x_test + x_adver).argmax(dim=1).detach().numpy().astype(int)
@@ -104,8 +99,7 @@ print(adver_clf_mat - perturbs_clf_mat)
 
 
 # %%
-np.savetxt("models/classification_results/mnist_cnn_testset.txt", y_pred)
-np.savetxt("models/classification_results/mnist_cnn_testset_adver.txt", y_pred_adver)
-np.savetxt("models/classification_results/mnist_cnn_testset_perturbs.txt", y_pred_perturbs)
+np.savetxt("models/classification_results/on_single_point/fcnn_model/cnn_testset_adver.txt", y_pred_adver)
+np.savetxt("models/classification_results/on_single_point/fcnn_model/cnn_testset_perturbs.txt", y_pred_perturbs)
 
 # %%
