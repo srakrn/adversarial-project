722ed698ecdb9a1094a9fa8fb8e6d2a7f1e07f41
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-02-12 21:00:13 +0700

    :sparkle: converted from Jupyter Notebook
    Former-commit-id: 15cc155ab1182748198cbd3534955294ffd0241c [formerly bcf7d3ff77173093b333da537b66d550ee8a2b6b]
    Former-commit-id: 575d8d0f50a1239676804e522bdb3fb738cf3290
    Former-commit-id: 8cf16f50eed89e10d7ab20f68c7007d8d664c995 [formerly cb3a8bf27751c0d1bf58ba6b2178969e37002b2e] [formerly c4f78b8e85dc70a293320734ae3db81e5c936a31 [formerly e9e35b4416566c398211c3d98a8b28505d0581a8]]
    Former-commit-id: 6feb592c182f710866014d8ada6be7071d81e14e [formerly da522a34df3533e984b9a2404d2195c68afc9ae6]
    Former-commit-id: 6456df4b6aafc522d2f6ffb8418b8e14c655cc00

--- codes/reinforcing/cluster_based/fcnn/reinforce_fcnn.py
+++ codes/reinforcing/cluster_based/fcnn/reinforce_fcnn.py
@@ -7,15 +7,16 @@ import torch.nn.functional as F
 from sklearn.cluster import KMeans
 from sklearn.metrics import classification_report
 from torch import nn, optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets, transforms
 
-#%%
+# %%
 transform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
 )
 
-#%%
+
+# %%
 mnist_trainset = datasets.MNIST(
     root="mnist", train=True, download=True, transform=transform
 )
@@ -23,10 +24,16 @@ mnist_testset = datasets.MNIST(
     root="mnist", train=False, download=True, transform=transform
 )
 
+
+# %%
 trainloader = DataLoader(mnist_trainset, batch_size=64, shuffle=True)
+full_trainloader = DataLoader(
+    mnist_trainset, batch_size=len(mnist_trainset), shuffle=True
+)
 testloader = DataLoader(mnist_testset, batch_size=1, shuffle=False)
 
-#%%
+
+# %%
 class MnistFcnn(nn.Module):
     def __init__(self):
         super(MnistFcnn, self).__init__()
@@ -42,27 +49,11 @@ class MnistFcnn(nn.Module):
         return x
 
 
-#%%
+# %%
 model = MnistFcnn()
+mnist_state = torch.load("models/mnist_fcnn.model")
+model.load_state_dict(mnist_state)
 
-#%%
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adagrad(model.parameters(), lr=0.03)
-
-#%%
-epochs = 10
-for e in range(epochs):
-    running_loss = 0
-    for images, labels in trainloader:
-        optimizer.zero_grad()
-        output = model(images)
-        loss = criterion(output, labels)
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.item()
-    else:
-        print(f"Training loss: {running_loss/len(trainloader)}")
 
 # %%
 y_test = []
@@ -73,18 +64,43 @@ for image, label in testloader:
 
 print(classification_report(y_test, y_pred))
 
+
 # %%
-n = 1000
-density = 0.2
+trainset_perturbs = torch.load(
+    "perturbs/on_single_point/fcnn_trainset_on_single_point.pt"
+)
+testset_perturbs = torch.load(
+    "perturbs/on_single_point/fcnn_testset_on_single_point.pt"
+)
+
+
+# %%
+y_test = []
+y_pred = []
+for (image, label), perturb in zip(testloader, trainset_perturbs):
+    y_test.append(label.item())
+    y_pred.append(
+        model(image + 0.2 * perturb.reshape(1, 1, 28, 28)).argmax(axis=1).item()
+    )
+
+print(classification_report(y_test, y_pred))
+
+
+# %%
+plt.imshow(
+    (image + 0.2 * perturb.reshape(1, 1, 28, 28)).reshape(28, 28).detach().numpy()
+)
+plt.show()
+
 
 # %%
 def calculate_k_perturbs(
-    model, training_set, k, n_epoches=20, verbose=False, log=False
+    model, training_set, clusterer, k, n_epoches=20, verbose=0, log=False
 ):
     loader = DataLoader(training_set, batch_size=len(training_set), shuffle=False)
     X, y = next(iter(loader))
-    km = KMeans(n_clusters=k)
-    km_clusters = km.fit_predict(X.reshape(len(X), -1))
+    km = KMeans(n_clusters=k, verbose=verbose, n_init=3)
+    km_clusters = km.fit_predict(clusterer.reshape(len(clusterer), -1))
     print(f"Training {k} perturbs")
 
     k_points = []
@@ -100,10 +116,10 @@ def calculate_k_perturbs(
 
         perturb = torch.zeros([1, 28 * 28], requires_grad=True)
         criterion = nn.CrossEntropyLoss()
-        optimizer = optim.SGD([perturb], lr=0.03)
+        optimizer = optim.Adagrad([perturb], lr=0.03)
 
         if verbose:
-            print(f"\tTraining #{i+1} perturb")
+            print(f"Training #{i+1} perturb")
             print(f"\tThis set of perturbation will attack {len(data)} data points.")
 
         for e in range(n_epoches):
@@ -131,29 +147,188 @@ def calculate_k_perturbs(
 
 
 # %%
-attack_targets, attack_perturbs, attack_clusters = calculate_k_perturbs(
-    model, mnist_trainset, n, verbose=True
+train_target, train_perturb, train_km = calculate_k_perturbs(
+    model, mnist_trainset, trainset_perturbs.detach().numpy(), 100, verbose=2
 )
+
+
+# %%
+test_target, test_perturb, test_km = calculate_k_perturbs(
+    model, mnist_testset, testset_perturbs.detach().numpy(), 100, verbose=2
+)
+
 
 # %%
 fig, axs = plt.subplots(10, 10, figsize=(10, 10))
-for ax, perturb in zip(axs.ravel(), attack_perturbs):
+for ax, perturb in zip(axs.ravel(), train_perturb):
     ax.imshow(perturb.reshape(28, 28))
 plt.show()
+
+
+# %%
+fig, axs = plt.subplots(10, 10, figsize=(10, 10))
+for ax, perturb in zip(axs.ravel(), test_perturb):
+    ax.imshow(perturb.reshape(28, 28))
+plt.show()
+
+
+# %%
+def get_nth_perturb(n, targets, perturbs):
+    for i, j in zip(targets, perturbs):
+        if i in targets:
+            return j
+    return None
+
 
 # %%
 y_test = []
 y_pred = []
 for i, (image, label) in enumerate(testloader):
-    for j, attack_target in enumerate(attack_targets):
-        if i in attack_target:
-            perturb = torch.tensor(attack_perturbs[j]).reshape(1, 1, 28, 28)
-            if i % 100 == 0:
-                print(i, j)
-    x = image + density * perturb
     y_test.append(label.item())
-    y_pred.append(model(x).argmax().item())
+    perturb = torch.tensor(get_nth_perturb(i, test_target, test_perturb))
+    y_pred.append(
+        model(image + 0.2 * perturb.reshape(1, 1, 28, 28)).argmax(axis=1).item()
+    )
 
 print(classification_report(y_test, y_pred))
+
+
+# %%
+class PerturbDataset(Dataset):
+    def __init__(self, data, targets, perturbs, density=0.2):
+        super().__init__()
+        self.data = data
+        self.targets = targets
+        self.perturbs = perturbs
+        self.density = density
+
+    def __len__(self):
+        return len(self.data)
+
+    def _get_nth_perturb(self, n):
+        for i, j in zip(self.targets, self.perturbs):
+            if i in self.targets:
+                return j
+        return None
+
+    def __getitem__(self, idx):
+        X, y = self.data[idx]
+        X += 0.2 * perturb.reshape(1, 28, 28)
+        return X, y
+
+
+# %%
+pd = PerturbDataset(mnist_trainset, train_target, train_perturb)
+
+
+# %%
+perturbloader = DataLoader(pd, batch_size=16, shuffle=True)
+
+
+# %%
+criterion = nn.CrossEntropyLoss(reduction="none")
+optimizer = optim.Adam(model.parameters())
+epochs = 10
+for e in range(epochs):
+    running_loss = 0
+    for (images, labels), (adver_images, adver_labels) in zip(
+        trainloader, perturbloader
+    ):
+        X = torch.cat([images, adver_images], 0)
+        y = torch.cat([labels, adver_labels], 0)
+        w = torch.tensor(
+            [
+                1 if i < len(labels) else 2
+                for i in range(len(labels) + len(adver_labels))
+            ]
+        ).float()
+        optimizer.zero_grad()
+
+        output = F.log_softmax(model(X), dim=1)
+        loss = torch.dot(criterion(output, y), w)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+    else:
+        print(f"Training loss: {running_loss/len(trainloader)}")
+
+
+# %%
+y_test = []
+y_pred = []
+for image, label in testloader:
+    y_test.append(label.item())
+    y_pred.append(model(image).argmax(axis=1).item())
+
+print(classification_report(y_test, y_pred))
+
+
+# %%
+y_test = []
+y_pred = []
+for (image, label), perturb in zip(testloader, trainset_perturbs):
+    y_test.append(label.item())
+    y_pred.append(
+        model(image + 0.2 * perturb.reshape(1, 1, 28, 28)).argmax(axis=1).item()
+    )
+
+print(classification_report(y_test, y_pred))
+
+
+# %%
+rem = MnistFcnn()
+
+
+# %%
+criterion = nn.CrossEntropyLoss(reduction="none")
+optimizer = optim.Adam(rem.parameters())
+epochs = 10
+for e in range(epochs):
+    running_loss = 0
+    for (images, labels), (adver_images, adver_labels) in zip(
+        trainloader, perturbloader
+    ):
+        X = torch.cat([images, adver_images], 0)
+        y = torch.cat([labels, adver_labels], 0)
+        w = torch.tensor(
+            [
+                1 if i < len(labels) else 2
+                for i in range(len(labels) + len(adver_labels))
+            ]
+        ).float()
+        optimizer.zero_grad()
+
+        output = F.log_softmax(rem(X), dim=1)
+        loss = torch.dot(criterion(output, y), w)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+    else:
+        print(f"Training loss: {running_loss/len(trainloader)}")
+
+
+# %%
+y_test = []
+y_pred = []
+for image, label in testloader:
+    y_test.append(label.item())
+    y_pred.append(model(image).argmax(axis=1).item())
+
+print(classification_report(y_test, y_pred))
+
+
+# %%
+y_test = []
+y_pred = []
+for (image, label), perturb in zip(testloader, trainset_perturbs):
+    y_test.append(label.item())
+    y_pred.append(
+        model(image + 0.2 * perturb.reshape(1, 1, 28, 28)).argmax(axis=1).item()
+    )
+
+print(classification_report(y_test, y_pred))
+
 
 # %%
