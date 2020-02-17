1c5fec1cd4cdf68088682937be77794e0761cef1
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-02-17 18:43:05 +0700

    :truck:

--- codes/attacking/on_single_point/cifar10/resnet_attack_trainset_fgsm.py
+++ codes/attacking/on_single_point/cifar10/resnet_attack_trainset_fgsm.py
@@ -1,11 +1,15 @@
 # %%
 import os
+import sys
 
 import torch
 from torch import nn, optim
 
-from attack import fgsm
-from cifar10_helpers import cifar10_resnet_model, cifar10_trainset
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from attack import fgsm  # isort:skip
+from cifar10_helpers import cifar10_resnet_model, cifar10_trainset  # isort:skip
 
 # %%
 OUTPUT_PATH = "perturbs/on_single_point/cifar10/resnet_fgsm_perturbs_trainset.pt"
