38be7dec1a2a83c5d7c7635d271f859fbc1f54b5
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-02-17 18:39:26 +0700

    Appending import paths

--- codes/attacking/on_single_point/mnist/fcnn_attack_trainset_maxloss.py
+++ codes/attacking/on_single_point/mnist/fcnn_attack_trainset_maxloss.py
@@ -1,11 +1,16 @@
 # %%
 import os
+import sys
 
 import torch
 from torch import nn, optim
 
-from attack import maxloss
-from mnist_helpers import mnist_fcnn_model, mnist_trainset
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from attack import maxloss  # isort:skip
+from mnist_helpers import mnist_fcnn_model, mnist_trainset  # isort:skip
+
 
 # %%
 OUTPUT_PATH = "perturbs/on_single_point/mnist/fcnn_maxloss_perturbs_trainset.pt"
