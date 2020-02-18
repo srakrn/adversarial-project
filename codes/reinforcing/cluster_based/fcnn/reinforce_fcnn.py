98d124525e6f91371e4b968a77466fbd0af9dae1
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-02-18 13:21:12 +0700

    :poop:

--- codes/reinforcing/cluster_based/fcnn/reinforce_fcnn.py
+++ codes/reinforcing/cluster_based/fcnn/reinforce_fcnn.py
@@ -59,24 +59,6 @@ train_target, train_perturb, train_km = reinforce.calculate_k_perturbs(
 )
 
 # %%
-"""
-test_target, test_perturb, test_km = reinforce.calculate_k_perturbs(
-    model, mnist_testset, testset_perturbs.detach().numpy(), k, attack_method="maxloss"
-)
-
-y_test = []
-y_pred = []
-for i, (image, label) in enumerate(testloader):
-    y_test.append(label.item())
-    perturb = torch.tensor(reinforce.get_nth_perturb(i, test_target, test_perturb))
-    y_pred.append(
-        model(image + 0.2 * perturb.reshape(1, 1, 28, 28)).argmax(axis=1).item()
-    )
-
-print(classification_report(y_test, y_pred))
-"""
-
-# %%
 ad = reinforce.AdversarialDataset(
     mnist_helpers.mnist_trainset, train_target, train_perturb
 )
@@ -109,59 +91,3 @@ for (image, label), perturb in zip(mnist_helpers.testloader, trainset_perturbs):
     )
 
 print(classification_report(y_test, y_pred))
-
-"""
-
-# %%
-rem = MnistFcnn()
-
-# %%
-criterion = nn.CrossEntropyLoss(reduction="none")
-optimizer = optim.Adam(rem.parameters())
-epochs = 10
-for e in range(epochs):
-    running_loss = 0
-    for (images, labels), (adver_images, adver_labels) in zip(
-        trainloader, perturbloader
-    ):
-        X = torch.cat([images, adver_images], 0)
-        y = torch.cat([labels, adver_labels], 0)
-        w = torch.tensor(
-            [
-                1 if i < len(labels) else 2
-                for i in range(len(labels) + len(adver_labels))
-            ]
-        ).float()
-        optimizer.zero_grad()
-
-        output = F.log_softmax(rem(X), dim=1)
-        loss = torch.dot(criterion(output, y), w)
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.item()
-    else:
-        print(f"Training loss: {running_loss/len(trainloader)}")
-
-
-# %%
-y_test = []
-y_pred = []
-for image, label in testloader:
-    y_test.append(label.item())
-    y_pred.append(model(image).argmax(axis=1).item())
-
-print(classification_report(y_test, y_pred))
-
-
-# %%
-y_test = []
-y_pred = []
-for (image, label), perturb in zip(testloader, trainset_perturbs):
-    y_test.append(label.item())
-    y_pred.append(
-        model(image + 0.2 * perturb.reshape(1, 1, 28, 28)).argmax(axis=1).item()
-    )
-
-print(classification_report(y_test, y_pred))
-"""
