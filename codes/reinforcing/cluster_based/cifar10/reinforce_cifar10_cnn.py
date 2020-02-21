d2eb42fb0604eea717ddd9de0686df63c4c22c35
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-02-21 12:33:58 +0700

    :bug:

--- codes/reinforcing/cluster_based/cifar10/reinforce_cifar10_cnn.py
+++ codes/reinforcing/cluster_based/cifar10/reinforce_cifar10_cnn.py
@@ -1,4 +1,5 @@
 # %%
+import logging
 import os
 import sys
 
@@ -12,47 +13,51 @@ from torchvision import datasets, transforms
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-import mnist_helpers  # isort:skip
+import cifar10_helpers  # isort:skip
 import reinforce  # isort:skip
 
-#%%
-model = mnist_helpers.mnist_resnet_model
-for i in model.parameters():
-    i.requires_grad = False
-model.fc.requires_grad = True
+logging.basicConfig(
+    filename=f"logs/{os.path.basename(__file__)}.log",
+    filemode="a",
+    level="INFO",
+    format="%(process)d-%(levelname)s-%(asctime)s-%(message)s",
+)
+
+# %%
+model = cifar10_helpers.cifar10_cnn_model
 
 # %%
 trainset_perturbs = torch.load(
-    "perturbs/on_single_point/mnist/fcnn_fgsm_perturbs_trainset.pt"
+    "perturbs/on_single_point/cifar10/cnn_attack_fgsm_trainset.pt"
 )
 testset_perturbs = torch.load(
-    "perturbs/on_single_point/mnist/fcnn_fgsm_perturbs_testset.pt"
+    "perturbs/on_single_point/cifar10/cnn_attack_fgsm_testset.pt"
 )
 
 # %%
 epsilon = 0.2
 
 # %%
-model.eval()
-# %%
 y_test = []
 y_pred = []
-for image, label in mnist_helpers.testloader:
+for image, label in cifar10_helpers.testloader:
     y_test.append(label.item())
     y_pred.append(model(image).argmax(axis=1).item())
 print("Original model report:")
 print(classification_report(y_test, y_pred))
+logging.info(classification_report(y_test, y_pred))
 
 # %%
 y_test = []
 y_pred = []
-for (image, label), perturb in zip(mnist_helpers.testloader, testset_perturbs):
+for (image, label), perturb in zip(cifar10_helpers.testloader, testset_perturbs):
     y_test.append(label.item())
     y_pred.append(
-        model(image + 0.2 * perturb.reshape(1, 1, 28, 28)).argmax(axis=1).item()
+        model(image + 0.2 * perturb.reshape(1, 3, 32, 32)).argmax(axis=1).item()
     )
 print("Adversarial on original model report:")
 print(classification_report(y_test, y_pred))
+logging.info(classification_report(y_test, y_pred))
 
 
 # %%
@@ -60,40 +65,41 @@ k = 100
 
 # %%
 train_target, train_perturb, train_km = reinforce.calculate_k_perturbs(
-    model, mnist_helpers.mnist_trainset, trainset_perturbs.detach().numpy(), k
+    model, cifar10_helpers.cifar10_trainset, trainset_perturbs.detach().numpy(), k
 )
 
 # %%
 ad = reinforce.AdversarialDataset(
-    mnist_helpers.mnist_trainset, train_target, train_perturb
+    cifar10_helpers.cifar10_trainset, train_target, train_perturb
 )
 adversarialloader = DataLoader(ad, batch_size=16, shuffle=True)
 
 # %%
-model.train()
-print(f"Started reinforcing on {reinforce.get_time()}")
+logging.info(f"Started reinforcing on {reinforce.get_time()}")
 reinforced_model = reinforce.k_reinforce(
-    model, mnist_helpers.trainloader, adversarialloader
+    model, cifar10_helpers.trainloader, adversarialloader, n_epoches=20
 )
-print(f"Finished reinforcing on {reinforce.get_time()}")
+logging.info(f"Finished reinforcing on {reinforce.get_time()}")
 
 # %%
 y_test = []
 y_pred = []
-for image, label in mnist_helpers.testloader:
+for image, label in cifar10_helpers.testloader:
     y_test.append(label.item())
     y_pred.append(model(image).argmax(axis=1).item())
 print("Reinforced model report:")
 print(classification_report(y_test, y_pred))
+logging.info(classification_report(y_test, y_pred))
 
 
 # %%
 y_test = []
 y_pred = []
-for (image, label), perturb in zip(mnist_helpers.testloader, trainset_perturbs):
+for (image, label), perturb in zip(cifar10_helpers.testloader, trainset_perturbs):
     y_test.append(label.item())
     y_pred.append(
-        model(image + 0.2 * perturb.reshape(1, 1, 28, 28)).argmax(axis=1).item()
+        model(image + 0.2 * perturb.reshape(1, 3, 32, 32)).argmax(axis=1).item()
     )
 print("Adversarial on reinforced model report:")
 print(classification_report(y_test, y_pred))
+logging.info(classification_report(y_test, y_pred))
