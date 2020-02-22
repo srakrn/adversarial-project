f8c975acbd4b06d0c26d0bad0ca5b0a639fa8fa7
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-02-22 08:15:21 -0500

    Merged

--- clustre/attacking/on_single_point/cifar10/resnet_attack_trainset_maxloss.py
+++ clustre/attacking/on_single_point/cifar10/resnet_attack_trainset_maxloss.py
@@ -6,11 +6,10 @@ import sys
 import torch
 from torch import nn, optim
 
+from clustre.helpers.cifar10_helpers import (  # isort:skip
+    cifar10_resnet_model, cifar10_testset)
+
 from clustre.attacking.on_single_point.attack import maxloss  # isort:skip
-from clustre.helpers.cifar10_helpers import (
-    cifar10_resnet_model,
-    cifar10_testset,
-)  # isort:skip
 
 logging.basicConfig(
     filename=f"logs/{os.path.basename(__file__)}.log",
@@ -25,7 +24,7 @@ OUTPUT_PATH = "perturbs/on_single_point/cifar10/resnet_attack_fgsm_testset.pt"
 # %%
 criterion = nn.CrossEntropyLoss()
 logging.info("Started running")
-perturbs = maxloss(cifar10_resnet_model, criterion, cifar10_testset, verbose=True)
+perturbs = maxloss(cifar10_resnet_model, criterion, trainloader, verbose=True)
 logging.info("Ended running")
 #  %%
 torch.save(perturbs, OUTPUT_PATH)
