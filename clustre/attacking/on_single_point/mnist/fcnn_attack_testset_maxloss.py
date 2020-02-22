01bf983cc0269e24b8d6c2496fbf1401a239007d
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-02-22 13:48:17 +0700

    Package style import

--- clustre/attacking/on_single_point/mnist/fcnn_attack_testset_maxloss.py
+++ clustre/attacking/on_single_point/mnist/fcnn_attack_testset_maxloss.py
@@ -6,11 +6,8 @@ import sys
 import torch
 from torch import nn, optim
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from attack import maxloss  # isort:skip
-from mnist_helpers import mnist_fcnn_model, mnist_testset  # isort:skip
+from clustre.attacking.on_single_point.attack import maxloss  # isort:skip
+from helpers.mnist_helpers import mnist_fcnn_model, mnist_testset  # isort:skip
 
 logging.basicConfig(
     filename=f"logs/{os.path.basename(__file__)}.log",
