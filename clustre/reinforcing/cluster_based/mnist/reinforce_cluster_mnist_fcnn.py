bff31e477ab7ebd51ff6e32e6693c51e5be857fc
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-02-22 22:04:05 -0500

    Refactor

--- clustre/reinforcing/cluster_based/mnist/reinforce_cluster_mnist_fcnn.py
+++ clustre/reinforcing/cluster_based/mnist/reinforce_cluster_mnist_fcnn.py
@@ -5,12 +5,12 @@ import sys
 
 import torch
 import torch.nn.functional as F
-from sklearn.metrics import classification_report
 from torch import nn, optim
 from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets, transforms
 
 from clustre.helpers import mnist_helpers
+from clustre.reinforcing.cluster_based import helpers as reinforce_helpers
 from clustre.reinforcing.cluster_based import reinforce
 
 logging.basicConfig(
@@ -24,80 +24,44 @@ logging.basicConfig(
 model = mnist_helpers.mnist_fcnn_model
 
 # %%
-trainset_perturbs = torch.load(
+trainset_fgsm_perturbs = torch.load(
     "perturbs/on_single_point/mnist/fcnn_fgsm_perturbs_trainset.pt"
 )
-testset_perturbs = torch.load(
+testset_fgsm_perturbs = torch.load(
     "perturbs/on_single_point/mnist/fcnn_fgsm_perturbs_testset.pt"
+)
+trainset_pgd_perturbs = torch.load(
+    "perturbs/on_single_point/mnist/fcnn_pgd_perturbs_trainset.pt"
+)
+testset_pgd_perturbs = torch.load(
+    "perturbs/on_single_point/mnist/fcnn_pgd_perturbs_testset.pt"
 )
 
 # %%
 epsilon = 0.2
-
-# %%
-y_test = []
-y_pred = []
-for image, label in mnist_helpers.testloader:
-    y_test.append(label.item())
-    y_pred.append(model(image).argmax(axis=1).item())
-print("Original model report:")
-print(classification_report(y_test, y_pred))
-logging.info(classification_report(y_test, y_pred))
-
-# %%
-y_test = []
-y_pred = []
-for (image, label), perturb in zip(mnist_helpers.testloader, testset_perturbs):
-    y_test.append(label.item())
-    y_pred.append(
-        model(image + 0.2 * perturb.reshape(1, 1, 28, 28)).argmax(axis=1).item()
-    )
-print("Adversarial on original model report:")
-print(classification_report(y_test, y_pred))
-logging.info(classification_report(y_test, y_pred))
-
-
-# %%
 k = 100
 
 # %%
-train_target, train_perturb, train_km = reinforce.calculate_k_perturbs(
-    model, mnist_helpers.mnist_trainset, trainset_perturbs.detach().numpy(), k, verbose=True
+reinforce_helpers.accuracy_unattacked(model, mnist_helpers.testloader)
+reinforce_helpers.accuracy_attacked(
+    model, mnist_helpers.testloader, testset_pgd_perturbs
 )
 
 # %%
+train_target, train_perturb, train_km = reinforce.calculate_k_perturbs(
+    model,
+    mnist_helpers.mnist_trainset,
+    trainset_pgd_perturbs.detach().numpy(),
+    k,
+    verbose=True,
+)
+
 ad = reinforce.AdversarialDataset(
     mnist_helpers.mnist_trainset, train_target, train_perturb
 )
 adversarialloader = DataLoader(ad, batch_size=16, shuffle=True)
 
-# %%
 trainloader = DataLoader(mnist_helpers.mnist_trainset, batch_size=32, shuffle=False)
 logging.info(f"Started reinforcing on {reinforce.get_time()}")
-reinforced_model = reinforce.k_reinforce(
-    model, trainloader, adversarialloader
-)
+reinforced_model = reinforce.k_reinforce(model, trainloader, adversarialloader)
 logging.info(f"Finished reinforcing on {reinforce.get_time()}")
-
-# %%
-y_test = []
-y_pred = []
-for image, label in mnist_helpers.testloader:
-    y_test.append(label.item())
-    y_pred.append(model(image).argmax(axis=1).item())
-print("Reinforced model report:")
-print(classification_report(y_test, y_pred))
-logging.info(classification_report(y_test, y_pred))
-
-
-# %%
-y_test = []
-y_pred = []
-for (image, label), perturb in zip(mnist_helpers.testloader, testset_perturbs):
-    y_test.append(label.item())
-    y_pred.append(
-        model(image + 0.2 * perturb.reshape(1, 1, 28, 28)).argmax(axis=1).item()
-    )
-print("Adversarial on reinforced model report:")
-print(classification_report(y_test, y_pred))
-logging.info(classification_report(y_test, y_pred))
--- clustre/reinforcing/cluster_based/mnist/reinforce_cluster_mnist_resnet.py
+++ clustre/reinforcing/cluster_based/mnist/reinforce_cluster_mnist_resnet.py
@@ -5,12 +5,12 @@ import sys
 
 import torch
 import torch.nn.functional as F
-from sklearn.metrics import classification_report
 from torch import nn, optim
 from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets, transforms
 
 from clustre.helpers import mnist_helpers
+from clustre.reinforcing.cluster_based import helpers as reinforce_helpers
 from clustre.reinforcing.cluster_based import reinforce
 
 logging.basicConfig(
@@ -19,93 +19,49 @@ logging.basicConfig(
     level="INFO",
     format="%(process)d-%(levelname)s-%(asctime)s-%(message)s",
 )
-# %%
-model = mnist_helpers.mnist_resnet_model
-for i in model.parameters():
-    i.requires_grad = False
-model.fc.weight.requires_grad = True
-model.fc.bias.requires_grad = True
 
 # %%
-trainset_perturbs = torch.load(
+model = mnist_helpers.mnist_resnet_model
+
+# %%
+trainset_fgsm_perturbs = torch.load(
     "perturbs/on_single_point/mnist/resnet_fgsm_perturbs_trainset.pt"
 )
-testset_perturbs = torch.load(
+testset_fgsm_perturbs = torch.load(
     "perturbs/on_single_point/mnist/resnet_fgsm_perturbs_testset.pt"
+)
+trainset_pgd_perturbs = torch.load(
+    "perturbs/on_single_point/mnist/resnet_pgd_perturbs_trainset.pt"
+)
+testset_pgd_perturbs = torch.load(
+    "perturbs/on_single_point/mnist/resnet_pgd_perturbs_testset.pt"
 )
 
 # %%
 epsilon = 0.2
-
-# %%
-model.eval()
-# %%
-y_test = []
-y_pred = []
-for image, label in mnist_helpers.testloader:
-    y_test.append(label.item())
-    y_pred.append(model(image).argmax(axis=1).item())
-print("Original model report:")
-print(classification_report(y_test, y_pred))
-logging.info(classification_report(y_test, y_pred))
-# %%
-y_test = []
-y_pred = []
-for (image, label), perturb in zip(mnist_helpers.testloader, testset_perturbs):
-    perturb = perturb.to("cpu")
-    y_test.append(label.item())
-    y_pred.append(
-        model(image + 0.2 * perturb.reshape(1, 1, 28, 28)).argmax(axis=1).item()
-    )
-print("Adversarial on original model report:")
-print(classification_report(y_test, y_pred))
-logging.info(classification_report(y_test, y_pred))
-
-
-# %%
 k = 100
 
 # %%
-train_target, train_perturb, train_km = reinforce.calculate_k_perturbs(
-    model, mnist_helpers.mnist_trainset, trainset_perturbs.cpu().detach().numpy(), k, verbose=True
+reinforce_helpers.accuracy_unattacked(model, mnist_helpers.testloader)
+reinforce_helpers.accuracy_attacked(
+    model, mnist_helpers.testloader, testset_pgd_perturbs
 )
 
 # %%
+train_target, train_perturb, train_km = reinforce.calculate_k_perturbs(
+    model,
+    mnist_helpers.mnist_trainset,
+    trainset_pgd_perturbs.detach().numpy(),
+    k,
+    verbose=True,
+)
+
 ad = reinforce.AdversarialDataset(
     mnist_helpers.mnist_trainset, train_target, train_perturb
 )
 adversarialloader = DataLoader(ad, batch_size=16, shuffle=True)
 
-# %%
-model.train()
 trainloader = DataLoader(mnist_helpers.mnist_trainset, batch_size=32, shuffle=False)
 logging.info(f"Started reinforcing on {reinforce.get_time()}")
-reinforced_model = reinforce.k_reinforce(
-    model, trainloader, adversarialloader, drop_last=True, adversarial_weight=5, cuda=True
-)
+reinforced_model = reinforce.k_reinforce(model, trainloader, adversarialloader)
 logging.info(f"Finished reinforcing on {reinforce.get_time()}")
-
-# %%
-model.eval()
-y_test = []
-y_pred = []
-for image, label in mnist_helpers.testloader:
-    y_test.append(label.item())
-    y_pred.append(model(image).argmax(axis=1).item())
-print("Reinforced model report:")
-print(classification_report(y_test, y_pred))
-logging.info(classification_report(y_test, y_pred))
-
-
-# %%
-y_test = []
-y_pred = []
-for (image, label), perturb in zip(mnist_helpers.testloader, testset_perturbs):
-    perturb = perturb.to("cpu")
-    y_test.append(label.item())
-    y_pred.append(
-        model(image + 0.2 * perturb.reshape(1, 1, 28, 28)).argmax(axis=1).item()
-    )
-print("Adversarial on reinforced model report:")
-print(classification_report(y_test, y_pred))
-logging.info(classification_report(y_test, y_pred))
