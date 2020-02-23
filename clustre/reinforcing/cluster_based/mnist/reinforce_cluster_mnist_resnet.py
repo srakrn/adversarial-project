95ccc8e30f833dfa39b1d6764910ed56c094330d
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-02-23 06:04:21 -0500

    Refactor

--- clustre/reinforcing/cluster_based/mnist/reinforce_cluster_mnist_resnet.py
+++ clustre/reinforcing/cluster_based/mnist/reinforce_cluster_mnist_resnet.py
@@ -10,8 +10,16 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets, transforms
 
 from clustre.helpers import mnist_helpers
-from clustre.reinforcing.cluster_based import helpers as reinforce_helpers
+from clustre.reinforcing import helpers as reinforce_helpers
 from clustre.reinforcing.cluster_based import reinforce
+
+# PARAMETERS
+EPSILON = 0.2
+N_CLUSTERS = 100
+EVALUATION_PERTURB = "pgd"
+CLUSTERING_PERTURB = "pgd"
+LEARNING_PERTURB = "pgd"
+
 
 logging.basicConfig(
     filename=f"logs/{os.path.basename(__file__)}.log",
@@ -25,43 +33,63 @@ model = mnist_helpers.mnist_resnet_model
 
 # %%
 trainset_fgsm_perturbs = torch.load(
-    "perturbs/on_single_point/mnist/resnet_fgsm_perturbs_trainset.pt"
+    "perturbs/on_single_point/mnist/fcnn_fgsm_perturbs_trainset.pt"
 )
 testset_fgsm_perturbs = torch.load(
-    "perturbs/on_single_point/mnist/resnet_fgsm_perturbs_testset.pt"
+    "perturbs/on_single_point/mnist/fcnn_fgsm_perturbs_testset.pt"
 )
 trainset_pgd_perturbs = torch.load(
-    "perturbs/on_single_point/mnist/resnet_pgd_perturbs_trainset.pt"
+    "perturbs/on_single_point/mnist/fcnn_pgd_perturbs_trainset.pt"
 )
 testset_pgd_perturbs = torch.load(
-    "perturbs/on_single_point/mnist/resnet_pgd_perturbs_testset.pt"
+    "perturbs/on_single_point/mnist/fcnn_pgd_perturbs_testset.pt"
 )
 
 # %%
-epsilon = 0.2
-k = 100
+logging.info(f"EPSILON = {EPSILON}")
+logging.info(f"N_CLUSTERS = {N_CLUSTERS}")
+logging.info(f"EVALUATION_PERTURB = {EVALUATION_PERTURB}")
+logging.info(f"LEARNING_PERTURB = {LEARNING_PERTURB}")
+
+if EVALUATION_PERTURB == "pgd":
+    evaluation_perturbs = testset_pgd_perturbs
+elif EVALUATION_PERTURB == "fgsm":
+    evaluation_perturbs = testset_fgsm_perturbs
+
+if CLUSTERING_PERTURB == "pgd":
+    clustering_perturbs = trainset_pgd_perturbs
+elif CLUSTERING_PERTURB == "fgsm":
+    clustering_perturbs = trainset_fgsm_perturbs
 
 # %%
 reinforce_helpers.accuracy_unattacked(model, mnist_helpers.testloader)
 reinforce_helpers.accuracy_attacked(
-    model, mnist_helpers.testloader, testset_pgd_perturbs
+    model, mnist_helpers.testloader, evaluation_perturbs
 )
 
 # %%
 train_target, train_perturb, train_km = reinforce.calculate_k_perturbs(
     model,
     mnist_helpers.mnist_trainset,
-    trainset_pgd_perturbs.detach().numpy(),
-    k,
+    clustering_perturbs.detach().cpu().numpy(),
+    N_CLUSTERS,
     verbose=True,
+    cuda=True,
+    attack_method=LEARNING_PERTURB
 )
 
 ad = reinforce.AdversarialDataset(
-    mnist_helpers.mnist_trainset, train_target, train_perturb
+    mnist_helpers.mnist_trainset, train_target, train_perturb, cuda=False
 )
 adversarialloader = DataLoader(ad, batch_size=16, shuffle=True)
 
 trainloader = DataLoader(mnist_helpers.mnist_trainset, batch_size=32, shuffle=False)
 logging.info(f"Started reinforcing on {reinforce.get_time()}")
-reinforced_model = reinforce.k_reinforce(model, trainloader, adversarialloader)
+reinforced_model = reinforce.k_reinforce(model, trainloader, adversarialloader, cuda=True)
 logging.info(f"Finished reinforcing on {reinforce.get_time()}")
+
+# %%
+reinforce_helpers.accuracy_unattacked(reinforced_model, mnist_helpers.testloader)
+reinforce_helpers.accuracy_attacked(
+    reinforced_model, mnist_helpers.testloader, evaluation_perturbs
+)
--- clustre/reinforcing/helpers.py
+++ clustre/reinforcing/helpers.py
@@ -6,9 +6,14 @@ log = logging.getLogger(__name__)
 
 
 def accuracy_unattacked(model, testloader):
+    model.eval()
     y_test = []
     y_pred = []
     for image, label in testloader:
+        if not image.is_cuda:
+            image = image.to("cuda")
+            label = label.to("cuda")
+        model.to("cuda")
         y_test.append(label.item())
         y_pred.append(model(image).argmax(axis=1).item())
     print("Original model report:")
@@ -17,14 +22,19 @@ def accuracy_unattacked(model, testloader):
 
 
 def accuracy_attacked(model, testloader, testset_perturbs, density=0.2):
+    model.eval()
     y_test = []
     y_pred = []
     for (image, label), perturb in zip(testloader, testset_perturbs):
-        if perturb.device == "cuda":
-            perturb = perturb.to("cpu")
+        if not image.is_cuda:
+            image = image.to("cuda")
+            label = label.to("cuda")
+        if not perturb.is_cuda:
+            perturb = perturb.to("cuda")
+        model.to("cuda")
         y_test.append(label.item())
         y_pred.append(
-            model(image + density * perturb.reshape(image.shape).unsqueeze_(0))
+            model(image + density * perturb.reshape(image.shape))
             .argmax(axis=1)
             .item()
         )
