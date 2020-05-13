133cdba71bd4a3b6519882ef5ef8e032e5f0d80c
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-05-13 13:52:01 +0700

    For free

--- clustre/adversarial_training/__init__.py
+++ clustre/adversarial_training/__init__.py
@@ -1,2 +1,3 @@
 from clustre.adversarial_training._fgsm import fgsm_training
+from clustre.adversarial_training._free import free_training
 from clustre.adversarial_training._pgd import pgd_training
