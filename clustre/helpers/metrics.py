d4d3750f167dd3f006fa1fce4132e087755db684
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-05-14 12:02:54 +0700

    :bug:

--- clustre/helpers/metrics.py
+++ clustre/helpers/metrics.py
@@ -28,11 +28,11 @@ def classification_report_fgsm(model, testloader, device=None, fgsm_params={}):
         if device is not None:
             images = images.to(device)
             labels = labels.to(device)
+        attacked_images = fgsm(
+            model, nn.CrossEntropyLoss(), images, labels, device=device, **fgsm_params
+        )
         y_true.append(labels)
-        y_pred.append(model(images))
-        perturbs = pgd(model, nn.CrossEntropyLoss(), images, labels, **fgsm_params)
-        y_true.append(labels)
-        y_pred.append(model(perturbs))
+        y_pred.append(model(attacked_images))
     y_true = torch.cat(y_true)
     y_pred = torch.cat(y_pred)
     y_pred = y_pred.argmax(dim=1)
@@ -47,13 +47,11 @@ def classification_report_pgd(model, testloader, device=None, pgd_params={}):
         if device is not None:
             images = images.to(device)
             labels = labels.to(device)
-        y_true.append(labels)
-        y_pred.append(model(images))
-        perturbs = pgd(
+        attacked_images = pgd(
             model, nn.CrossEntropyLoss(), images, labels, device=device, **pgd_params
         )
         y_true.append(labels)
-        y_pred.append(model(perturbs))
+        y_pred.append(model(attacked_images))
     y_true = torch.cat(y_true)
     y_pred = torch.cat(y_pred)
     y_pred = y_pred.argmax(dim=1)
