e4a99f4789da2e1c41e5b8909ebe04db9c5deb5f
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-05-21 13:41:54 +0700

    :beers:

--- clustre/adversarial_training/_cluster.py
+++ clustre/adversarial_training/_cluster.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch
 from sklearn.cluster import KMeans
 from torch import nn, optim
@@ -20,7 +19,6 @@ class AdversarialDataset(Dataset):
         criterion=nn.CrossEntropyLoss(),
         density=0.3,
         n_clusters=100,
-        ratio=0.4,
         kmeans_parameters={},
         pgd_parameters={},
         transform=None,
@@ -39,11 +37,7 @@ class AdversarialDataset(Dataset):
         self.km.fit(d)
         # Obtain targets and ids of each cluster centres
         self.cluster_ids = self.km.predict(d)
-        self.nearest_cluster_dist = self.km.transform(d).min(axis=1)
-        n_items = int(len(d) * ratio)
-        self.cluster_centers_idx = np.argpartition(
-            self.nearest_cluster_dist, n_items
-        )[:n_items]
+        self.cluster_centers_idx = self.km.transform(d).argmin(axis=0)
 
         # Extract only interested ones
         X = []
@@ -73,7 +67,7 @@ def cluster_training(
     trainloader,
     n_epoches=10,
     n_clusters=100,
-    sample_ratio=0.1,
+    sample_ratio=0.5,
     epsilon=0.3,
     criterion=nn.CrossEntropyLoss(),
     optimizer=optim.Adam,
@@ -81,7 +75,7 @@ def cluster_training(
     pgd_step_size=0.02,
     fgsm_parameters={},
     kmeans_parameters={"n_init": 3},
-    pgd_parameters={"n_epoches": 7},
+    pgd_parameters={},
     device=None,
     log=None,
 ):
@@ -118,7 +112,7 @@ def cluster_training(
             centroids_y,
             epsilon=epsilon,
             step_size=pgd_step_size,
-            **pgd_parameters,
+            n_epoches=n_epoches,
         )
         # Running loss, for reference
         running_loss = 0
