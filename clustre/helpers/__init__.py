8f43027253af6b9a694d617f721e73e3e8821b4b
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-05-22 22:03:27 +0700

    Method rename

--- clustre/helpers/__init__.py
+++ clustre/helpers/__init__.py
@@ -1,1 +1,1 @@
-from clustre.helpers._time import get_time
+from clustre.helpers._time import format_time, get_time, get_time_string
--- clustre/helpers/_time.py
+++ clustre/helpers/_time.py
@@ -1,6 +1,19 @@
+import warnings
 from datetime import datetime
 
 
-def get_time():
+def get_time_string():
     t = datetime.now()
-    return t.strftime("%m/%d/%Y, %H:%M:%S")
+    return format_time(t)
+
+
+def get_time():
+    warnings.warn(
+        "This method is deprecated. Use get_time_string() instead.",
+        warnings.DeprecationWarning,
+    )
+    return get_time()
+
+
+def format_time(t):
+    return t.strftime("%m/%d/%Y, %H:%M:%S.%f")
