7a4575bd4e117bb8ff1233a47ec11d944b259a79
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-05-22 22:13:02 +0700

    Reverted API change

--- clustre/helpers/__init__.py
+++ clustre/helpers/__init__.py
@@ -1,1 +1,1 @@
-from clustre.helpers._time import format_time, get_time, get_time_string
+from clustre.helpers._time import format_time, get_time
--- clustre/helpers/_time.py
+++ clustre/helpers/_time.py
@@ -2,17 +2,9 @@ import warnings
 from datetime import datetime
 
 
-def get_time_string():
+def get_time():
     t = datetime.now()
     return format_time(t)
-
-
-def get_time():
-    warnings.warn(
-        "This method is deprecated. Use get_time_string() instead.",
-        warnings.DeprecationWarning,
-    )
-    return get_time()
 
 
 def format_time(t):
