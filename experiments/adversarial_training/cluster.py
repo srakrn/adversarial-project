40de07a91fe832c2402f563d54c87e242520a013
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-05-22 10:35:58 +0700

    :beers:

--- experiments/adversarial_training/cluster.py
+++ experiments/adversarial_training/cluster.py
@@ -36,7 +36,7 @@ from clustre.models.state_dicts import (
 )
 
 # %%
-DEVICE = torch.cuda.device(1)
+DEVICE = "cuda:0"
 LOG_FILENAME = os.path.abspath(__file__)[:-3] + "_log.txt"
 SCRIPT_PATH = os.path.dirname(__file__)
 FORMAT = "%(message)s"
@@ -53,30 +53,37 @@ cifar10_wideresnet.load_state_dict(cifar10_wideresnet_state)
 
 models = {
     "MNIST ResNet": [mnist_resnet, mnist_trainloader, mnist_testloader],
-    "CIFAR-10 ResNet": [
-        cifar10_resnet,
-        cifar10_trainloader,
-        cifar10_testloader,
-    ],
 }
+
+params = [
+    {"n_clusters": 10},
+    {"n_clusters": 50},
+    {"n_clusters": 100},
+    {"n_clusters": 500},
+    {"n_clusters": 1000},
+]
 
 new_models = {}
 
 # %%
 for model_name, (model, trainloader, testloader) in models.items():
-    logging.info(f"Training {model_name}")
-    new_model = cluster_training(model, trainloader, device=DEVICE, log=log)
-    torch.save(
-        model.state_dict(),
-        os.path.join(SCRIPT_PATH, f"Cluster{model_name}.model"),
-    )
+    for p in params:
+        logging.info(f"Training {model_name}")
+        logging.info(f"Params {p}")
+        new_model = cluster_training(
+            model, trainloader, device=DEVICE, log=log, **p
+        )
+        torch.save(
+            model.state_dict(),
+            os.path.join(SCRIPT_PATH, f"Cluster{model_name}.model"),
+        )
 
-    logging.info(f"FGSM attacked {model_name}")
-    logging.info(
-        classification_report_fgsm(new_model, testloader, device=DEVICE)
-    )
+        logging.info(f"FGSM attacked {model_name}")
+        logging.info(
+            classification_report_fgsm(new_model, testloader, device=DEVICE)
+        )
 
-    logging.info(f"PGD attacked {model_name}")
-    logging.info(
-        classification_report_pgd(new_model, testloader, device=DEVICE)
-    )
+        logging.info(f"PGD attacked {model_name}")
+        logging.info(
+            classification_report_pgd(new_model, testloader, device=DEVICE)
+        )
