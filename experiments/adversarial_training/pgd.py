6752a08a3fa170761b45ddcc4742bda81cabceb8
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-05-28 17:24:17 +0000

    Introducing unattacked benchmark

--- experiments/adversarial_training/pgd.py
+++ experiments/adversarial_training/pgd.py
@@ -4,8 +4,6 @@ import os
 import sys
 
 import torch
-from torch import nn, optim
-
 from clustre.adversarial_training import pgd_training
 from clustre.helpers.datasets import (cifar10_testloader, cifar10_trainloader,
                                       mnist_testloader, mnist_trainloader)
@@ -15,10 +13,9 @@ from clustre.helpers.metrics import (classification_report,
 from clustre.models import (cifar10_cnn, cifar10_resnet, cifar10_wideresnet,
                             mnist_cnn, mnist_fcnn, mnist_resnet)
 from clustre.models.state_dicts import (cifar10_cnn_state,
-                                        cifar10_resnet_state,
-                                        cifar10_wideresnet_state,
-                                        mnist_cnn_state, mnist_fcnn_state,
+                                        cifar10_resnet_state, mnist_cnn_state,
                                         mnist_resnet_state)
+from torch import nn, optim
 
 # %%
 LOG_FILENAME = os.path.abspath(__file__)[:-3] + "_log.txt"
@@ -28,15 +25,12 @@ logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO, format=FORMAT)
 log = logging.getLogger()
 
 # %%
-mnist_fcnn.load_state_dict(mnist_fcnn_state)
 mnist_cnn.load_state_dict(mnist_cnn_state)
 mnist_resnet.load_state_dict(mnist_resnet_state)
 cifar10_cnn.load_state_dict(cifar10_cnn_state)
 cifar10_resnet.load_state_dict(cifar10_resnet_state)
-cifar10_wideresnet.load_state_dict(cifar10_wideresnet_state)
 
 models = {
-    "MNIST ResNet": [mnist_resnet, mnist_trainloader, mnist_testloader],
     "CIFAR-10 ResNet": [
         cifar10_resnet,
         cifar10_trainloader,
@@ -44,17 +38,19 @@ models = {
     ],
 }
 
-new_models = {}
-
 # %%
 for model_name, (model, trainloader, testloader) in models.items():
     logging.info(f"Training {model_name}")
-    new_model = pgd_training(model, trainloader, device="cuda", log=log)
-    new_models[model_name] = new_model
+    new_model = pgd_training(
+        model, trainloader, n_epoches=40, device="cuda", log=log
+    )
     torch.save(
         model.state_dict(),
         os.path.join(SCRIPT_PATH, f"PGD {model_name}.model"),
     )
+
+    logging.info(f"Unattacked {model_name}")
+    logging.info(classification_report(model, testloader, device="cuda"))
 
     logging.info(f"FGSM attacked {model_name}")
     logging.info(classification_report_fgsm(model, testloader, device="cuda"))
