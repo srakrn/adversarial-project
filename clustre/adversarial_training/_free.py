565aaac3bc1c83dae89ae7fc83e9bd6ec713b036
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-05-29 12:55:25 +0700

    Non-bugging for free!

--- clustre/adversarial_training/_free.py
+++ clustre/adversarial_training/_free.py
@@ -27,32 +27,6 @@ def free_training(
     device=None,
     log=None,
 ):
-    """Adversarial Training For Free
-
-    Parameters
-    ----------
-    model: torch.nn.model
-        The model to be reinforced
-    trainloader: torch.utils.data.DataLoader
-        The DataLoader for the training set
-    n_epoches: int
-        The epoches to be trained
-    epsilon: float
-        Perturbation bound
-    hop_step: int
-        Hop step
-    criterion: function
-        Criterion function
-    optimizer: class of torch.optim
-        Optimiser to train the model
-    optimizer_params: dict
-        Parameters to be passed to the optimiser
-    device: torch.device, str, or None
-        Device to be used
-    log: logger or None
-        If logger, logs to the corresponding logger
-    """
-
     # Move to device if desired
     if device is not None:
         model.to(device)
@@ -62,6 +36,7 @@ def free_training(
 
     # Create an optimiser instance
     optimizer = optimizer(model.parameters(), **optimizer_params)
+    delta = None
 
     # Iterate over e times of epoches
     for e in range(math.ceil(n_epoches / hop_step)):
@@ -76,15 +51,17 @@ def free_training(
             if device is not None:
                 images = images.to(device)
                 labels = labels.to(device)
-            images.requires_grad = True
 
             # Initialise attacking images
-            delta = torch.zeros(images.shape, requires_grad=True, device=images.device)
+            if delta is None:
+                delta = torch.zeros(
+                    images.shape, requires_grad=True, device=images.device
+                )
 
             # Replay each minibatch for `hop_steps` to simulate PGD
             for i in range(hop_step):
                 # Initialise attacking images
-                attack_images = images + delta
+                attack_images = (images + delta).detach().requires_grad_()
 
                 # Update model's parameter
                 optimizer.zero_grad()
@@ -96,9 +73,9 @@ def free_training(
                 optimizer.step()
 
                 # Use gradients calculated for the minimisation step to update delta
-                perturbs = epsilon * delta.grad.data.sign()
-                delta = torch.clamp(delta, min=-epsilon, max=epsilon).detach()
-                delta.requires_grad = True
+                grad = attack_images.grad.data.detach()
+                delta = delta.detach() + epsilon * torch.sign(grad)
+                delta.clamp_(min=-epsilon, max=epsilon)
 
             running_loss += loss.item()
         else:
@@ -106,4 +83,5 @@ def free_training(
                 log.info(f"\tTraining loss: {running_loss/len(trainloader)}")
     if log is not None:
         log.info(f"Training ended: {get_time()}")
+
     return model
