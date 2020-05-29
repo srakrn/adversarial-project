6764d521ad5df1f8980eecc586ef3417429f2874
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-05-29 13:11:24 +0700

    Free training experiment

---
+++ experiments/adversarial_training/free.py
@@ -0,0 +1,60 @@
+# %%
+import logging
+import os
+
+import torch
+
+from clustre.adversarial_training import free_training
+from clustre.helpers.datasets import (cifar10_testloader, cifar10_trainloader,
+                                      mnist_testloader, mnist_trainloader)
+from clustre.helpers.metrics import (classification_report,
+                                     classification_report_fgsm,
+                                     classification_report_pgd)
+from clustre.models import cifar10_cnn, cifar10_resnet, mnist_cnn, mnist_resnet
+from clustre.models.state_dicts import (cifar10_cnn_state,
+                                        cifar10_resnet_state, mnist_cnn_state,
+                                        mnist_resnet_state)
+
+# %%
+LOG_FILENAME = os.path.abspath(__file__)[:-3] + "_log.txt"
+SCRIPT_PATH = os.path.dirname(__file__)
+FORMAT = "%(message)s"
+logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO, format=FORMAT)
+log = logging.getLogger()
+
+# %%
+mnist_cnn.load_state_dict(mnist_cnn_state)
+mnist_resnet.load_state_dict(mnist_resnet_state)
+cifar10_cnn.load_state_dict(cifar10_cnn_state)
+cifar10_resnet.load_state_dict(cifar10_resnet_state)
+
+models = {
+    "MNIST CNN": [mnist_cnn, mnist_trainloader, mnist_testloader],
+    "MNIST ResNet": [mnist_resnet, mnist_trainloader, mnist_testloader],
+    "CIFAR-10 CNN": [cifar10_cnn, cifar10_trainloader, cifar10_testloader],
+    "CIFAR-10 ResNet": [
+        cifar10_resnet,
+        cifar10_trainloader,
+        cifar10_testloader,
+    ],
+}
+
+# %%
+for model_name, (model, trainloader, testloader) in models.items():
+    logging.info(f"Training {model_name}")
+    new_model = free_training(
+        model, trainloader, n_epoches=40, device="cuda", log=log
+    )
+    torch.save(
+        model.state_dict(),
+        os.path.join(SCRIPT_PATH, f"PGD {model_name}.model"),
+    )
+
+    logging.info(f"Unattacked {model_name}")
+    logging.info(classification_report(model, testloader, device="cuda"))
+
+    logging.info(f"FGSM attacked {model_name}")
+    logging.info(classification_report_fgsm(model, testloader, device="cuda"))
+
+    logging.info(f"PGD attacked {model_name}")
+    logging.info(classification_report_pgd(model, testloader, device="cuda"))
