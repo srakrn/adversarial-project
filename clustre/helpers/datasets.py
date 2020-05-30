6b3a4292e76c47cf2be3c357e9102f086ce7f2e1
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-05-30 18:13:33 +0000

    Decreasing test set batch size

--- clustre/helpers/datasets.py
+++ clustre/helpers/datasets.py
@@ -47,9 +47,9 @@ cifar10_testset = datasets.CIFAR10(
 )
 
 mnist_trainloader = DataLoader(mnist_trainset, batch_size=128, shuffle=True)
-mnist_testloader = DataLoader(mnist_testset, batch_size=128, shuffle=True)
+mnist_testloader = DataLoader(mnist_testset, batch_size=32, shuffle=True)
 
 cifar10_trainloader = DataLoader(
     cifar10_trainset, batch_size=128, shuffle=True
 )
-cifar10_testloader = DataLoader(cifar10_testset, batch_size=128, shuffle=True)
+cifar10_testloader = DataLoader(cifar10_testset, batch_size=32, shuffle=True)
