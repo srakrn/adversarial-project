80fb7959e0b3865f0959128fed4a1fcbad27df71
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-07-08 16:34:24 +0700

    Random FGSM

--- clustre/attacking/_fgsm.py
+++ clustre/attacking/_fgsm.py
@@ -1,14 +1,30 @@
 import torch
 
 
-def fgsm(model, criterion, image, label, epsilon=0.3, device=None):
+def fgsm(
+    model,
+    criterion,
+    image,
+    label,
+    epsilon=0.3,
+    random=False,
+    alpha=0.1,
+    device=None,
+):
     if device is not None:
         model.to(device)
         image = image.to(device)
         label = label.to(device)
 
     perturb = fgsm_perturbs(
-        model, criterion, image, label, epsilon=epsilon, device=device
+        model,
+        criterion,
+        image,
+        label,
+        epsilon=epsilon,
+        random=random,
+        alpha=alpha,
+        device=device,
     )
     attack_image = torch.clamp(image + perturb, min=-1, max=1)
 
